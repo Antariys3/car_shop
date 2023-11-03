61c0fba5d8aef0200bdafb5c520f8dd0f62203ed
antar <333antar333@gmail.com>
2023-11-03 20:06:34 +0100

    Renamed the project to car_shop

--- Car_shop/asgi.py
+++ Car_shop/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Carshop project.
+ASGI config for Car_shop project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Carshop.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Car_shop.settings")
 
 application = get_asgi_application()
--- Car_shop/urls.py
+++ Car_shop/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Carshop project.
+URL configuration for Car_shop project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
