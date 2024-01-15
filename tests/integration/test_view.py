a8e1661aa4ff36b768eb7daa6080d3b9551aed05
antar <333antar333@gmail.com>
2024-01-15 16:23:44 +0100

    Added a view to check payment status and wrote tests

--- tests/integration/test_view.py
+++ tests/integration/test_view.py
@@ -10,46 +10,46 @@ import re
 SERVER_URL = "https://boiling-fortress-51276-88bb58822abe.herokuapp.com/api/"
 
 
-# def test_get_api_cars():
-#     r = requests.get(SERVER_URL + "cars/")
-#     r.raise_for_status()
-#     assert r.status_code == 200
+def test_get_api_cars():
+    r = requests.get(SERVER_URL + "cars/")
+    r.raise_for_status()
+    assert r.status_code == 200
 
-# def test_get_api_car_by_id():
-#     car_id = "100"
-#     expected_url = f"{SERVER_URL}cars/{car_id}"
-#     expected_response = {
-#         "id": 100,
-#         "car_type": {
-#             "id": 102,
-#             "name": "Astra",
-#             "brand": "Opel",
-#             "price": 9000,
-#             "image": re.compile(r'https://cars-shop-aws\.s3\.amazonaws\.com/Images_of_cars/Opel_Astra_blue\.png.*')
-#         },
-#         "color": "синий",
-#         "year": 2009
-#     }
-#
-#     try:
-#         response = requests.get(expected_url)
-#         response.raise_for_status()
-#     except HTTPError as e:
-#         pytest.fail(f"Request failed with status code {e.response.status_code}")
-#
-#     assert response.status_code == 200
-#     assert expected_response["car_type"]["image"].match(response.json()["car_type"]["image"])
-#     expected_response["car_type"]["image"] = response.json()["car_type"]["image"]
-#     assert response.json() == expected_response
+def test_get_api_car_by_id():
+    car_id = "100"
+    expected_url = f"{SERVER_URL}cars/{car_id}"
+    expected_response = {
+        "id": 100,
+        "car_type": {
+            "id": 102,
+            "name": "Astra",
+            "brand": "Opel",
+            "price": 9000,
+            "image": re.compile(r'https://cars-shop-aws\.s3\.amazonaws\.com/Images_of_cars/Opel_Astra_blue\.png.*')
+        },
+        "color": "синий",
+        "year": 2009
+    }
+
+    try:
+        response = requests.get(expected_url)
+        response.raise_for_status()
+    except HTTPError as e:
+        pytest.fail(f"Request failed with status code {e.response.status_code}")
+
+    assert response.status_code == 200
+    assert expected_response["car_type"]["image"].match(response.json()["car_type"]["image"])
+    expected_response["car_type"]["image"] = response.json()["car_type"]["image"]
+    assert response.json() == expected_response
 
 @pytest.mark.django_db
 def test_add_to_cart_api():
-    # Создаем тестового пользователя
+
     user = User.objects.create_user(username='testuser', password='testpassword')
 
-    # Создаем тестовую машину
+
     car = Car.objects.create(
-        car_type_id=110,  # Замените на реальный ID типа машины
+        car_type_id=110,
         color='Red',
         year=2022
     )
