c40bef3b02fa54aa38638f72e77baf1baf309786
antar <333antar333@gmail.com>
2024-04-08 22:17:40 +0200

    Push_heroku

--- carshop/invoices.py
+++ carshop/invoices.py
@@ -8,7 +8,9 @@ from django.conf import settings
 from carshop.models import Order
 
 PUBLIC_KEY = "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZrd0V3WUhLb1pJemowQ0FRWUlLb1pJemowREFRY0RRZ0FFc05mWXpNR1hIM2VXVHkzWnFuVzVrM3luVG5CYgpnc3pXWnhkOStObEtveDUzbUZEVTJONmU0RlBaWmsvQmhqamgwdTljZjVFL3JQaU1EQnJpajJFR1h3PT0KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg=="
-REDIRECT_URL = "REDIRECT_URL', 'https://boiling-fortress-51276-88bb58822abe.herokuapp.com/payment_status/"
+REDIRECT_URL = (
+    "https://boiling-fortress-51276-88bb58822abe.herokuapp.com/payment_status/"
+)
 
 
 def verify_signature(request):
