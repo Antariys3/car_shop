6ca843bd231a0e3f51eb023e383d5bfa2fe957eb
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-13 23:51:16 +0300

    file with constants was created. it helps to keep constants in the same place

---
+++ carshop/constants.py
@@ -0,0 +1,8 @@
+ASPECT_RATIO = 16 / 9
+
+REDIRECT_URL = (
+    "https://boiling-fortress-51276-88bb58822abe.herokuapp.com/payment_status/"
+)
+
+# TODO below key can not be stored in this file
+PUBLIC_KEY = "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZrd0V3WUhLb1pJemowQ0FRWUlLb1pJemowREFRY0RRZ0FFc05mWXpNR1hIM2VXVHkzWnFuVzVrM3luVG5CYgpnc3pXWnhkOStObEtveDUzbUZEVTJONmU0RlBaWmsvQmhqamgwdTljZjVFL3JQaU1EQnJpajJFR1h3PT0KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg=="
